39e66166df924b6d15e122a225be854e1860b02f
Dilpreet271 <kaurdilpreet271@gmail.com>
2022-04-17 22:26:07 +0530

    Question 1 and 2 Committed

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+//This file is used for making server side configurations using express framework This file is the base or main file to run node server     
+// Main sever.js file for node configurations
+
+/*make a constant variable --> express
+*and call external express module using require function 
+*and import external express module using 'require' function
+*/ 
 const express = require('express');
+
+/*Making path variable and import path module 
+ *for joinning multiple segments  
+*/
 const path = require('path');
+
 const app = express();
+//GET method is used that carries request parameter appended in URL
 
 app.get('/', function(req, res){
    res.sendFile(path.join(__dirname, '/homePage.html'));
@@ -10,10 +23,14 @@ app.get('/about', function(req, res){
     res.sendFile(path.join(__dirname, '/index.html'));
  });
 
-//app.get('/contact', function(req, res){
- // res.sendFile(path.join(dirname, '/contact.html'));
-//});
+app.use(express.static(__dirname+'/views')); // To import static files like CSS files
 
-app.use(express.static(__dirname+'/views'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.listen(3000);
+
+/*App.listen() is used to bind or listen the connections on specific port 
+*in this case it is at port -> 3000
+*/
+
+app.listen(3000); 
+
+
