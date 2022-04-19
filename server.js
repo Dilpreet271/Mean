10e7de1e72b06cc436e24ce6957f4ac28b9f509a
Dilpreet271 <kaurdilpreet271@gmail.com>
2022-04-19 11:21:36 +0530

    Complete ASsignment

--- server.js
+++ server.js
@@ -1,3 +1,45 @@
+//Ques 1 and 2
+
+//This file is used for making server side configurations using express framework This file is the base or main file to run node server     
+// Main sever.js file for node configurations
+
+/*make a constant variable --> express
+*and call external express module using require function 
+*and import external express module using 'require' function
+*/ 
+const express = require('express');
+
+/*Making path variable and import path module 
+ *for joinning multiple segments  
+*/
+const path = require('path');
+
+const app = express();
+//GET method is used that carries request parameter appended in URL
+
+app.get('/', function(req, res){
+   res.sendFile(path.join(__dirname, '/homePage.html'));
+});
+
+app.get('/about', function(req, res){
+    res.sendFile(path.join(__dirname, '/index.html'));
+ });
+
+app.use(express.static(__dirname+'/views')); // To import static files like CSS files
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*App.listen() is used to bind or listen the connections on specific port 
+*in this case it is at port -> 3000
+*/
+
+app.listen(3000); 
+
+
+
+
+//Ques 3
+
 //This file is used for making server side configurations using express framework This file is the base or main file to run node server     
 // Main sever.js file for node configurations
 
@@ -28,4 +70,6 @@ res.render('home', {data:data});
 var server = app.listen(4000, function() {
 	console.log('listening to port 4000')
 });
+
+
 
